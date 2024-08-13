74cfda9a94cb4fa80581fa7ad703bb93516326e4
actioninja <actioninja@criticalaction.net>
2024-08-13 03:34:20 -0700

    bump version and direct to other repo

--- internal/env/env.go
+++ internal/env/env.go
@@ -3,10 +3,10 @@ package env
 const Undefined = "undefined"
 
 var (
-	Title    = "StrongDMM-TALLHACK"
+	Title    = "StrongDMM-TALLHACKV2"
 	Version  = Undefined
 	Revision = Undefined
-	GitHub   = "https://github.com/SpaiR/StrongDMM"
+	GitHub   = "https://github.com/actioninja/StrongDMM/tree/tallhack"
 	Manifest = "https://spair.github.io/StrongDMM/manifest.json"
 	Support  = "https://ko-fi.com/spair"
 )
